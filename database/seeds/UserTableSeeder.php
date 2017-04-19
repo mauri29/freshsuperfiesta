e56f88b57d39e6cb0e64afe501caadc9adf1e076
Oscar Ramirez <Mauri@Oscars-Mac-mini.local>
2017-04-19 19:12:26 +0200

    login

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,17 +12,6 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         $user = new \App\Models\User([
-            'name' => 'Super',
-            'surname' => 'Fiesta',
-            'email' => 'superfiesta.ch@gmail.com',
-            'password' => '$2y$10$rAVjXlQL1VY6IQ72o71G8.gFs9dXaOrNOBOQN25Ebe6wI4m/sroXi',
-            'image_filename' => '',
-            'remember_token' => '',
-            'created_at' => '2017-04-08 04:24:58',
-            'updated_at' => '2017-04-10 02:23:58',
-        ]);
-        $user->save();
-        $user = new \App\Models\User([
             'name' => 'Mauricio',
             'surname' => 'Robayo',
             'email' => 'robayo.mauri@gmail.com',
