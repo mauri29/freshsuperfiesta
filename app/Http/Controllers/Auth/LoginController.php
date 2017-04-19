65f6214d5ee0d2005e96e6d1425405ad8da0e3b1
Oscar Ramirez <Mauri@Oscars-Mac-mini.local>
2017-04-19 22:19:33 +0200

    product

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Lang;
+
 class LoginController extends Controller
 {
     /*
