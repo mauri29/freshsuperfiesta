aa1a07fbbc365a354c23346c7690fb97f066e022
Oscar Ramirez <Mauri@Oscars-Mac-mini.local>
2017-04-20 13:51:44 +0200

    status

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (env('APP_ENV') === 'production') {
-            $this->app['request']->server->set('HTTPS', true);
-        }
+        //
     }
 }
